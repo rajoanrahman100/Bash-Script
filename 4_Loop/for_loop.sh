e90058b2625120ff8a65430f22db7abd68182a7c
rifat <rifat@tikweb.dk>
2024-12-17 17:20:12 +0600

    for loops

---
+++ 4_Loop/for_loop.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo ""
+echo "Print 1 to 5"
+for i in {1..5}
+do
+ echo $i
+done
+
+echo ""
+echo "Print the files in current directory"
+
+for num in $(ls *.txt)
+do
+    echo "Even number: $num"
+done
+
+echo ""
+echo "Bash also supports a C-style for loop (similar to C or Java syntax)"
+
+for (( i=1; i<=10; i++ ))
+do
+ echo $i
+done
