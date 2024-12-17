1f93cff9061e736570e83ff908c59d5e709bd45d
rifat <rifat@tikweb.dk>
2024-12-17 15:41:32 +0600

    if else statement

--- 3_ConditionalStatements/if_else_fi.sh
+++ 3_ConditionalStatements/if_else_fi.sh
@@ -4,3 +4,15 @@
 if else elif statements
 -----------------------
 comments
+
+echo "Enter a number:"
+read num
+if [ $num -gt 0 ]
+then
+ echo "$num is Positive"
+elif [ $num -lt 0 ]
+then
+ echo "$num is Negative"
+else 
+ echo "$num is zero"
+fi
