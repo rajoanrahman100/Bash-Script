39c99906ccdddfb6e46dab6fcee8b41c3503f8ff
rifat <rifat@tikweb.dk>
2024-12-20 17:25:35 +0600

    grep commands

--- 8_Bash_Others/grep_command.sh
+++ 8_Bash_Others/grep_command.sh
@@ -9,4 +9,8 @@ grep -iv "Rifat" names.txt # Show the results that are not matches
 grep -l "Rifat" * # Find all files that match "Rifat" pattern
 grep -w "Rifat" names.txt # Find files name along with the result that matches with "Rifat"
 grep -x "Rifat" names.txt # Print the line that only has "Rifat" pattern
-
+grep -f fileOne fileTwo # -f file option takes patterns from file, one per line
+grep -A1 "bristy" names.txt # Print after one line matching pattern
+grep -B1 "bristy" names.txt # Print before one line matching pattern
+grep -C2 "Bristy" names.txt # Print 2 line after and 2 line before matching pattern
+grep -C1 "Bristy" names.txt # Print 1 line after and 1 line before matching pattern
